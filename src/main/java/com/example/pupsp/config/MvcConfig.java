fb3ea771894452d6ed959242ea02bfc8dfab0881
Bryan <bryamcortine17@gmail.com>
2024-11-24 13:14:21 -0500

    Agregue el security y todo funciona correctamente

---
+++ src/main/java/com/example/pupsp/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.pupsp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("build/index");
+		registry.addViewController("/").setViewName("build/index");
+		registry.addViewController("/login").setViewName("build/login");
+		registry.addViewController("/register").setViewName("build/register");
+	}
+
+}
